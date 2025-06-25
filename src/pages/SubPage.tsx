bbd637b0725da340465c59098df9e648215e54ff
배지호 <blueapple99@naver.com>
2025-06-26 02:01:12 +0900

    feat: 상태 관리 라이브러리 설정
    - zustand 사용

--- src/pages/SubPage.tsx
+++ src/pages/SubPage.tsx
@@ -1,5 +1,15 @@
+import useStore from '../states/Ex1';
+
 const SubPage = () => {
-  return <div>hello</div>;
+  const { count, increment, decrement } = useStore();
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>증가</button>
+      <button onClick={decrement}>감소</button>
+    </div>
+  );
 };
 
 export default SubPage;
